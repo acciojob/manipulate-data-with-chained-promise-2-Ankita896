800525d6a173d4c884ecda16847e763209c08e79
Ankita896 <ankita143ankyy@gmail.com>
2025-08-03 22:47:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+//your JS code here. If required.
+const outputDiv = document.getElementById("output");
+
+const initialPromise = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve([1, 2, 3, 4]);
+    }, 3000);
+});
+
+initialPromise
+    .then((numbers) => {
+        const evenNumbers = numbers.filter((num) => num % 2 === 0);
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                outputDiv.textContent = evenNumbers.join(",");
+                resolve(evenNumbers);
+            }, 1000);
+        });
+    })
+    .then((evenNumbers) => {
+        const multipliedNumbers = evenNumbers.map((num) => num * 2);
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                outputDiv.textContent = multipliedNumbers.join(",");
+                resolve();
+            }, 2000);
+        });
+    });
+
